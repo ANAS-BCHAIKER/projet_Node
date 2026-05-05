905763f7ee1232b230d924075e50a51428c5c619
anasbchyker <anasbchyker@gmail.com>
2026-05-05 22:41:43 +0100

    lesson3

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile('./view/home.html', { root: __dirname });
-});
+//Connexion à la base de données
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://nodeJS:qYz4ZtwKW6gJRluj@cluster0.udxide4.mongodb.net/all-data?appName=Cluster0')
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', (req, res) => {
+    res.sendFile('./view/home.html', { root: __dirname });
 });
