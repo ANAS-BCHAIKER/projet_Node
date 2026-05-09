58397e765ac04fc3da23b4eb048a569aed1dc992
anasbchyker <anasbchyker@gmail.com>
2026-05-09 23:52:45 +0100

    Edit/Delete

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 var moment = require('moment');
+var methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 
 //Auto refresh
@@ -28,14 +30,14 @@ liveReloadServer.server.once("connection", () => {
 app.get("/", (req, res) => {
 
     User.find()
-    .then((result) => {
-        console.log('Users fetched from MongoDB:', result);
-        res.render('index', {arr: result, moment: moment});
-    })
-    .catch((err) => {
-        console.log('Error fetching users from MongoDB:', err);
-        res.render('index', {arr: []});
-    });
+        .then((result) => {
+            console.log('Users fetched from MongoDB:', result);
+            res.render('index', { arr: result, moment: moment });
+        })
+        .catch((err) => {
+            console.log('Error fetching users from MongoDB:', err);
+            res.render('index', { arr: [] });
+        });
 });
 
 app.get('/user/add.html', (req, res) => {
@@ -46,24 +48,32 @@ app.get('/user/add.html', (req, res) => {
 app.get("/", (req, res) => {
 
     User.find()
-    .then((result) => {
-        console.log('Users fetched from MongoDB:', result);
-        res.render('index', {arr: result});
-    })
-    .catch((err) => {
-        console.log('Error fetching users from MongoDB:', err);
-        res.render('index', {arr: []});
-    });
+        .then((result) => {
+            res.render('index', { arr: result });
+        })
+        .catch((err) => {
+            res.render('index', { arr: [] });
+        });
 });
 
-app.get('/user/:id', (req, res) => {
+app.get('/edit/:id', (req, res) => {
     User.findById(req.params.id)
     .then((result) => {
-        res.render('user/view', {obj: result, moment: moment});
+        res.render('user/edit', { obj: result, moment: moment });
     })
     .catch((err) => {
         res.send('User not found');
     });
+});
+
+app.get('/view/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then((result) => {
+            res.render('user/view', { obj: result, moment: moment });
+        })
+        .catch((err) => {
+            res.send('User not found');
+        });
 });
 
 //Post routes
@@ -79,6 +89,16 @@ app.post('/user/add.html', (req, res) => {
         });
 });
 
+app.delete('/edit/:id', (req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch((err) => {
+            console.error('Error deleting user from MongoDB:', err);
+            res.send('Error deleting user');
+        });
+});
 //Connexion à la base de données
 const mongoose = require('mongoose');
 const User = require('./models/customerSchema');
